ba084b6e31d470a65c147928d55c1a617b6cc9b7
Andrey Listopadov <andreyorst@gmail.com>
2022-12-09 18:42:08 +0300

    specify compile command for emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((cider-clojure-cli-aliases . ":dev"))))
+((nil . ((compile-command . "clojure ")
+         (cider-clojure-cli-aliases . ":dev"))))
