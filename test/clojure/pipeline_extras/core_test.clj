5a2e62451c806e41594397eba2ae9cf45ec7c8cb
Andrey Listopadov <andreyorst@gmail.com>
2022-12-09 18:40:04 +0300

    more tests

--- test/clojure/pipeline_extras/core_test.clj
+++ test/clojure/pipeline_extras/core_test.clj
@@ -8,11 +8,11 @@
    [pipeline-extras.core
     :refer
     [pipeline
+     pipeline-unordered
      pipeline-async
      pipeline-async-unordered
      pipeline-blocking
-     pipeline-blocking-unordered
-     pipeline-unordered]]))
+     pipeline-blocking-unordered]]))
 
 (defn- take-all!!
   ([c] (<!! (a/into [] c)))
@@ -39,8 +39,7 @@
 
 (deftest task-count-test
   (let [max 100
-        timeout 1000
-        parallelism 10
+        timeout 10000
         async-task (fn [x c]
                      (go
                        (swap! counter incr)
@@ -54,49 +53,51 @@
                        (swap! counter decr)
                        x)]
     (testing "Pipelines don't spawn more tasks than needed"
-      (let [to (chan parallelism)]
-        (reset-counter!)
-        (pipeline parallelism to (map compute-task) (to-chan! (range max)))
-        (is (= (set (range max))
-               (set (take-all!! to timeout))))
-        (is (= {:max parallelism :current 0}
-               @counter)))
-      (let [to (chan parallelism)]
-        (reset-counter!)
-        (pipeline-blocking parallelism to (map compute-task) (to-chan! (range max)))
-        (is (= (set (range max))
-               (set (take-all!! to timeout))))
-        (is (= {:max parallelism :current 0}
-               @counter)))
-      (let [to (chan parallelism)]
-        (reset-counter!)
-        (pipeline-async parallelism to async-task (to-chan! (range max)))
-        (is (= (set (range max))
-               (set (take-all!! to timeout))))
-        (is (= {:max parallelism :current 0}
-               @counter))))
+      (doseq [parallelism (range 1 10)]
+        (let [to (chan parallelism)]
+          (reset-counter!)
+          (pipeline parallelism to (map compute-task) (to-chan! (range max)))
+          (is (= (set (range max))
+                 (set (take-all!! to timeout))))
+          (is (= {:max parallelism :current 0}
+                 @counter)))
+        (let [to (chan parallelism)]
+          (reset-counter!)
+          (pipeline-blocking parallelism to (map compute-task) (to-chan! (range max)))
+          (is (= (set (range max))
+                 (set (take-all!! to timeout))))
+          (is (= {:max parallelism :current 0}
+                 @counter)))
+        (let [to (chan parallelism)]
+          (reset-counter!)
+          (pipeline-async parallelism to async-task (to-chan! (range max)))
+          (is (= (set (range max))
+                 (set (take-all!! to timeout))))
+          (is (= {:max parallelism :current 0}
+                 @counter)))))
     (testing "Unordered pipelines don't spawn more tasks than needed"
-      (let [to (chan parallelism)]
-        (reset-counter!)
-        (pipeline-unordered parallelism to (map compute-task) (to-chan! (range max)))
-        (is (= (set (range max))
-               (set (take-all!! to timeout))))
-        (is (= {:max parallelism :current 0}
-               @counter)))
-      (let [to (chan parallelism)]
-        (reset-counter!)
-        (pipeline-blocking-unordered parallelism to (map compute-task) (to-chan! (range max)))
-        (is (= (set (range max))
-               (set (take-all!! to timeout))))
-        (is (= {:max parallelism :current 0}
-               @counter)))
-      (let [to (chan parallelism)]
-        (reset-counter!)
-        (pipeline-async-unordered parallelism to async-task (to-chan! (range max)))
-        (is (= (set (range max))
-               (set (take-all!! to timeout))))
-        (is (= {:max parallelism :current 0}
-               @counter))))))
+      (doseq [parallelism (range 1 10)]
+        (let [to (chan parallelism)]
+          (reset-counter!)
+          (pipeline-unordered parallelism to (map compute-task) (to-chan! (range max)))
+          (is (= (set (range max))
+                 (set (take-all!! to timeout))))
+          (is (= {:max parallelism :current 0}
+                 @counter)))
+        (let [to (chan parallelism)]
+          (reset-counter!)
+          (pipeline-blocking-unordered parallelism to (map compute-task) (to-chan! (range max)))
+          (is (= (set (range max))
+                 (set (take-all!! to timeout))))
+          (is (= {:max parallelism :current 0}
+                 @counter)))
+        (let [to (chan parallelism)]
+          (reset-counter!)
+          (pipeline-async-unordered parallelism to async-task (to-chan! (range max)))
+          (is (= (set (range max))
+                 (set (take-all!! to timeout))))
+          (is (= {:max parallelism :current 0}
+                 @counter)))))))
 
 (deftest order-test
   (let [max 10
@@ -137,3 +138,76 @@
         (pipeline-blocking-unordered parallelism to (map compute-task) (to-chan! (range max)))
         (is (= (reverse (range max))
                (take-all!! to timeout)))))))
+
+(deftest transducer-test
+  (testing "all values from mapcat are available"
+    (let [to (chan 1)]
+      (pipeline 1 to (mapcat (partial repeat 2)) (to-chan! (range 10)))
+      (is (= (mapcat (partial repeat 2) (range 10))
+             (take-all!! to 100))))
+    (let [to (chan 1)]
+      (pipeline-unordered 1 to (mapcat (partial repeat 2)) (to-chan! (range 10)))
+      (is (= (mapcat (partial repeat 2) (range 10))
+             (take-all!! to 100))))))
+
+(deftest exception-test
+  (testing "exceptions in comute/blocking pipelines"
+    (let [to (chan 1)]
+      (pipeline
+       1 to
+       (map (fn [x] (throw (ex-info "exc" {:data x}))))
+       (to-chan! (range 10))
+       true (fn [e] (-> e ex-data :data)))
+      (is (= [0 1 2 3 4 5 6 7 8 9]
+             (take-all!! to 100))))
+    (let [to (chan 1)]
+      (pipeline-blocking
+       1 to
+       (map (fn [x] (throw (ex-info "exc" {:data x}))))
+       (to-chan! (range 10))
+       true (fn [e] (-> e ex-data :data)))
+      (is (= [0 1 2 3 4 5 6 7 8 9]
+             (take-all!! to 100)))))
+  (testing "exceptions in comute/blocking pipelines without a handler"
+    (let [to (chan 1)]
+      (pipeline
+       1 to
+       (map (fn [x] (throw (ex-info "exc" {:data x}))))
+       (to-chan! [1]))
+      (is (= [] (take-all!! to 100))))
+    (let [to (chan 1)]
+      (pipeline-blocking
+       1 to
+       (map (fn [x] (throw (ex-info "exc" {:data x}))))
+       (to-chan! [1]))
+      (is (= [] (take-all!! to 100)))))
+  (testing "exceptions in unordered comute/blocking pipelines"
+    (let [to (chan 1)]
+      (pipeline-unordered
+       1 to
+       (map (fn [x] (throw (ex-info "exc" {:data x}))))
+       (to-chan! (range 10))
+       true (fn [e] (-> e ex-data :data)))
+      (is (= [0 1 2 3 4 5 6 7 8 9]
+             (take-all!! to 100))))
+    (let [to (chan 1)]
+      (pipeline-blocking-unordered
+       1 to
+       (map (fn [x] (throw (ex-info "exc" {:data x}))))
+       (to-chan! (range 10))
+       true (fn [e] (-> e ex-data :data)))
+      (is (= [0 1 2 3 4 5 6 7 8 9]
+             (take-all!! to 100)))))
+  (testing "exceptions in unordered comute/blocking pipelines without a handler"
+    (let [to (chan 1)]
+      (pipeline-unordered
+       1 to
+       (map (fn [x] (throw (ex-info "exc" {:data x}))))
+       (to-chan! [1]))
+      (is (= [] (take-all!! to 100))))
+    (let [to (chan 1)]
+      (pipeline-blocking-unordered
+       1 to
+       (map (fn [x] (throw (ex-info "exc" {:data x}))))
+       (to-chan! [1]))
+      (is (= [] (take-all!! to 100))))))
